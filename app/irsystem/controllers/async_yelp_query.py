0f8ebc5a61cb7228c5b3c3399e1f61dc5f67e491
Maddie Franke <mmf239@cornell.edu>
2021-04-17 13:43:12 -0400

    yelp queries work with multiple locations + preferences

--- app/irsystem/controllers/async_yelp_query.py
+++ app/irsystem/controllers/async_yelp_query.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 
 import asyncio
-import time
+import time 
 from gql import Client, gql
 from gql.transport.aiohttp import AIOHTTPTransport
 
-places = ['''zion''', '''grand canyon''', '''sedona''']
-interests = ['''food''', '''activities''']
+#places = ['''zion''','''grand canyon''','''sedona''']
+interests = ['''food''','''activities''']
 
-
-def getQuery(interest, place):
+def getQuery(interest,place):
     query = gql('''{
         search(term:"''' + interest + '''",
             location:"''' + place + '''",
@@ -29,37 +28,37 @@ def getQuery(interest, place):
     ''')
     return query
 
-
-async def execute_query(session, interest, place):
-    result = await session.execute(getQuery(interest, place))
-    print(result)
+async def execute_query(session, interest, place, preferences):
+    result = await session.execute(getQuery(interest,place))
+    # call tims function
+    # return similarity(result)
+    # return top 3 businesses 
+    return result["search"]["business"][0]["name"]
 
 
 # Then create a couroutine which will connect to your API and run all your queries as tasks.
 # We use a `backoff` decorator to reconnect using exponential backoff in case of connection failure.
 
-# @backoff.on_exception(backoff.expo, Exception, max_time=300)
-async def graphql_connection():
-
+#@backoff.on_exception(backoff.expo, Exception, max_time=300)
+async def graphql_connection(places, preferences):
+    results = []
     api_key = "cnLXWAkkm24bIO09xAVZv9IPWhXo7TJwSjNKU7EZ2LdzBc8EwGC1gs6eP5-sn-u5Vt-6mzEBK_szWjk4NWDRYv-sC-AO43cFjp-Z_K7Up652-upZ4JB-GinH9pVvYHYx"
 
     # define our authentication process.
     header = {'Authorization': 'bearer {}'.format(api_key),
-              'Content-Type': "application/json"}
-    transport = AIOHTTPTransport(
-        url="https://api.yelp.com/v3/graphql", headers=header)
+            'Content-Type': "application/json"}
+    transport = AIOHTTPTransport(url="https://api.yelp.com/v3/graphql", headers=header)
 
     client = Client(transport=transport, fetch_schema_from_transport=True)
     async with client as session:
         for spot in places:
-            task1 = asyncio.create_task(
-                execute_query(session, interests[0], spot))
-            task2 = asyncio.create_task(
-                execute_query(session, interests[1], spot))
+            task1 = asyncio.create_task(execute_query(session,interests[0],spot, preferences)) 
+            task2 = asyncio.create_task(execute_query(session,interests[1],spot, preferences)) 
 
-        await asyncio.gather(task1, task2)
+            results.append(await asyncio.gather(task1, task2))
+        return results
 
-start = time.time()
-asyncio.run(graphql_connection())
-end = time.time()
-print(end-start)
+def get_request(place, preferences):
+    print(place)
+    print(preferences)
+    return asyncio.run(graphql_connection(place, preferences))
--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,20 +1,23 @@
 from . import *  
+from app.irsystem.controllers.asynch_yelp_query import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
-from app.irsystem.controllers import asynch_yelp_query as yelp_query
 
 project_name = "Road Trip Recommender"
 net_id = "Rohan Upadhyayula: ru38 Timothy Reeves: tj3239 Maddie Franke: mmf239 Cindy Pan: cyp7"
 
 @irsystem.route('/', methods=['GET'])
 def search():
-	query = request.args.get('search')
-	if not query:
+	locations = request.args.get('locations')
+	preferences = request.args.get('preferences')
+	if not locations:
 		data = []
 		output_message = ''
 	else:
-		output_message = "Your search: " + query
-		data = range(5)
+		output_message = "Your search: " + locations
+		data = get_request(locations.split(", "), preferences)
+		print(data)
+
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
 
