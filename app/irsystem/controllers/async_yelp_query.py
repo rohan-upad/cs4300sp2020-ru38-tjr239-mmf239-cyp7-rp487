de13dc166f28412867d1458e666296e760e61d7c
Cindy Pan <cindypan@dhcp-vl2042-22695.redrover.cornell.edu>
2021-04-30 17:36:08 -0400

    improved similarity measure

--- app/irsystem/controllers/async_yelp_query.py
+++ app/irsystem/controllers/async_yelp_query.py
@@ -30,13 +30,14 @@ def getAPIKey():
     data = json.load(f)
     return random.choice(list(data.values()))
 
-def getQuery(interest,place):
+def getQuery(interest,place, offset):
     query = gql('''{
         search(term:"''' + interest + '''",
             location:"''' + place + '''",
             radius: 40000,
             limit: 50,
-            sort_by: "rating") {
+            sort_by: "rating",
+            offset: ''' + offset + ''') {
                 total
                 business {
                     name
@@ -46,6 +47,9 @@ def getQuery(interest,place):
                     display_phone
                     url
                     photos
+                    categories {
+                        title
+                    }
                     reviews {
                         text
                         rating
@@ -57,18 +61,34 @@ def getQuery(interest,place):
     return query
 
 async def execute_query(session, interest, place, preferences):
-    result = await session.execute(getQuery(interest,place)) 
+    result = await session.execute(getQuery(interest,place,'''0''')) 
+    result2 = await session.execute(getQuery(interest,place,'''50'''))
     countlist = []
     for business in range(len(result['search']['business'])):
         counter = 0
         merged_reviews = ""
+        merged_reviews += result['search']['business'][business]['name']
         for review in range(len(result['search']['business'][business]['reviews'])):
             merged_reviews += result['search']['business'][business]['reviews'][review]['text']
+        for categories in range(len(result['search']['business'][business]['categories'])):
+            merged_reviews += " " + result['search']['business'][business]['categories'][categories]['title']
         for pref in preferences:
             counter+= merged_reviews.count(pref)
-        countlist.append((counter, result['search']['business'][business]['name']))
+        countlist.append((counter, result['search']['business'][business]['name'], result['search']['business'][business]['url']))
+    for business in range(len(result2['search']['business'])):
+        counter = 0
+        merged_reviews = ""
+        merged_reviews += result['search']['business'][business]['name']
+        for review in range(len(result2['search']['business'][business]['reviews'])):
+            merged_reviews += result2['search']['business'][business]['reviews'][review]['text']
+        for categories in range(len(result2['search']['business'][business]['categories'])):
+            merged_reviews += " " + result2['search']['business'][business]['categories'][categories]['title']
+        for pref in preferences:
+            counter+= merged_reviews.count(pref)
+        denom = len(merged_reviews.split(' ')) + 1 + len(preferences)
+        countlist.append((counter/denom, result2['search']['business'][business]['name'], result2['search']['business'][business]['url']))
     countlist.sort(key=lambda tup: tup[0], reverse=True)
-    new_list = [ seq[1] for seq in countlist]
+    new_list = [seq[1:] for seq in countlist]
     return new_list[:3]
 
 # Then create a couroutine which will connect to your API and run all your queries as tasks.
@@ -98,15 +118,15 @@ async def graphql_connection(places, preferences):
                 results.append(ans[2:])
             else:
                 results.append(await asyncio.gather(task1, task2))
-            
         return results
 
 def get_request(place, preferences):
-    new_preferences = preferences
+    new_preferences = []
     for pref in preferences:
         similar_words = get_prefs(pref)
         if similar_words:
             new_preferences.extend(similar_words)
+    new_preferences.extend(preferences)
     new_preferences = list(set(new_preferences))
     return asyncio.run(graphql_connection(place, new_preferences))
 
